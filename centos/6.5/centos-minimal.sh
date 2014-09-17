52a85396233119f5f9ee7382d29328b7af616c0d
root <root@hogthrob.dmz.alces-software.com>
2014-09-17 16:52:44 +0100

    - use specific config

--- centos/6.5/centos-minimal.sh
+++ centos/6.5/centos-minimal.sh
@@ -1,2 +1,2 @@
-oz-install -d3 -u centos-minimal.tdl -x centos-minimal.xml -p -a centos-minimal.auto
+oz-install -d3 -u centos-minimal.tdl -x centos-minimal.xml -p -a centos-minimal.auto -c ../../config/oz.cfg
 virt-sysprep -a /var/lib/libvirt/images/centos6.5-minimal.qcow2
--- centos/7.0/centos-minimal.sh
+++ centos/7.0/centos-minimal.sh
@@ -1,2 +1,2 @@
-oz-install -d3 -u centos-minimal.tdl -x centos-minimal.xml -p -a centos-minimal.auto
+oz-install -d3 -u centos-minimal.tdl -x centos-minimal.xml -p -a centos-minimal.auto -c ../../config/oz.cfg
 virt-sysprep -a /var/lib/libvirt/images/centos7.0-minimal.qcow2
