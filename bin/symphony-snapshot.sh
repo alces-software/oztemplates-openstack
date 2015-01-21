cded110feb61030dba626efdacc3183b2996e75b
root <root@hogthrob.dmz.alces-software.com>
2015-01-21 19:26:55 +0000

    - bugfix

--- bin/symphony-snapshot.sh
+++ bin/symphony-snapshot.sh
@@ -1,4 +1,4 @@
-ORIGINAL_VERSION=$1
+OLD_VERSION=$1
 NEW_VERSION=$2
 
 if [ -z "$NEW_VERSION" ]; then
@@ -11,9 +11,9 @@ fi
 
 POOL_DIR=/opt/vm
 
-virt-clone -o symphony-director-$NEW_VERSION -n symphony-director-$NEW_VERSION -f $POOL_DIR/symphony-director-$NEW_VERSION.qcow2 --force --replace
+virt-clone -o symphony-director-$OLD_VERSION -n symphony-director-$NEW_VERSION -f $POOL_DIR/symphony-director-$NEW_VERSION.qcow2 --force --replace
 virt-sysprep --enable abrt-data,bash-history,blkid-tab,crash-data,cron-spool,dhcp-client-state,dovecot-data,firstboot,logfiles,lvm-uuids,machine-id,mail-spool,net-hostname,net-hwaddr,pacct-log,package-manager-cache,pam-data,random-seed,rhn-systemid,rpm-db,samba-db-log,script,smolt-uuid,ssh-hostkeys,sssd-db-log,udev-persistent-net,utmp,yum-uuid --domain symphony-director-$NEW_VERSION
-virt-clone -o symphony-monitor-$NEW_VERSION -n symphony-monitor-$NEW_VERSION -f $POOL_DIR/symphony-monitor-$NEW_VERSION.qcow2 --force --replace
+virt-clone -o symphony-monitor-$OLD_VERSION -n symphony-monitor-$NEW_VERSION -f $POOL_DIR/symphony-monitor-$NEW_VERSION.qcow2 --force --replace
 virt-sysprep --enable abrt-data,bash-history,blkid-tab,crash-data,cron-spool,dhcp-client-state,dovecot-data,firstboot,logfiles,lvm-uuids,machine-id,mail-spool,net-hostname,net-hwaddr,pacct-log,package-manager-cache,pam-data,random-seed,rhn-systemid,rpm-db,samba-db-log,script,smolt-uuid,ssh-hostkeys,sssd-db-log,udev-persistent-net,utmp,yum-uuid --domain symphony-monitor-$NEW_VERSION
-virt-clone -o symphony-repo-$NEW_VERSION -n symphony-repo-$NEW_VERSION -f $POOL_DIR/symphony-repo-$NEW_VERSION.qcow2 -f $POOL_DIR/symphony-repo-$NEW_VERSION-pulp.qcow2 -f $POOL_DIR/symphony-repo-$NEW_VERSION-mongo.qcow2 --force --replace
+virt-clone -o symphony-repo-$OLD_VERSION -n symphony-repo-$NEW_VERSION -f $POOL_DIR/symphony-repo-$NEW_VERSION.qcow2 -f $POOL_DIR/symphony-repo-$NEW_VERSION-pulp.qcow2 -f $POOL_DIR/symphony-repo-$NEW_VERSION-mongo.qcow2 --force --replace
 virt-sysprep --enable abrt-data,bash-history,blkid-tab,crash-data,cron-spool,dhcp-client-state,dovecot-data,firstboot,logfiles,lvm-uuids,machine-id,mail-spool,net-hostname,net-hwaddr,pacct-log,package-manager-cache,pam-data,random-seed,rhn-systemid,rpm-db,samba-db-log,script,smolt-uuid,ssh-hostkeys,sssd-db-log,udev-persistent-net,utmp,yum-uuid --domain symphony-repo-$NEW_VERSION
